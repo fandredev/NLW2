e6d7b08b524044a071f6f5a0ba7e5919ee58f893
Felipe <profissionalf.andre@gmail.com>
2020-08-07 14:45:20 -0300

    feat: add aplication mobile

---
+++ app/src/components/PageHeader/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Image, Text } from "react-native";
+import styles from "./styles";
+
+import { BorderlessButton } from "react-native-gesture-handler";
+import backIcon from "../../assets/images/icons/back.png";
+import logoIcon from "../../assets/images/logo.png";
+import { useNavigation } from "@react-navigation/native";
+
+interface PageHeaderProps<S> {
+  title: S;
+}
+
+const PageHeader: React.FC<PageHeaderProps<string>> = ({ title }) => {
+  const { navigate } = useNavigation();
+  function handleGoBack() {
+    navigate("Landing");
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+        <BorderlessButton onPress={handleGoBack}>
+          <Image source={backIcon} resizeMode="contain" />
+        </BorderlessButton>
+        <Image source={logoIcon} resizeMode="contain" />
+      </View>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+};
+export default PageHeader;
---
+++ app/src/components/PageHeader/styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#8257e5",
+    padding: 40,
+  },
+  topBar: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontFamily: "Archivo_700Bold",
+    color: "#fff",
+    fontSize: 22,
+    maxWidth: 200,
+    lineHeight: 32,
+  },
+});
+export default styles;
