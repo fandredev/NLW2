f951090c4255caae020bbab70fd39ad8b4e6686a
Felipe <profissionalf.andre@gmail.com>
2020-08-08 14:31:26 -0300

    fix: removed all bugs in web

--- web/app/src/services/api.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://192.168.15.10:3333",
-});
-export default api;
--- web/src/pages/TeacherList/validations.ts
+++ web/src/pages/TeacherList/validations.ts
@@ -1,7 +1,9 @@
 import * as Yup from "yup";
 
 export const schema = Yup.object().shape({
-  subject: Yup.string().required("Matéria precisa ser obrigatório"),
-  week_day: Yup.string().required("Dia da semana precisa ser obrigatório"),
-  time: Yup.string().required("Hora precisa ser obrigatório"),
+  subject: Yup.string().required("O campo máteria precisa ser preenchido"),
+  week_day: Yup.string().required(
+    "O campo dia da semana precisa ser preenchido"
+  ),
+  time: Yup.string().required("O campo hora precisa ser preenchido"),
 });
